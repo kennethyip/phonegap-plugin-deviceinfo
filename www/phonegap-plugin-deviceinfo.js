a4eca861d3229e4dcafeb87e0bf2678b96fad5a3
kennethyip <kenneth.yip@tungwah.org.hk>
2014-03-31 11:15:27 +0800

    Modify config file

--- www/phonegap-plugin-deviceinfo.js
+++ www/phonegap-plugin-deviceinfo.js
@@ -1,13 +1,13 @@
 var deviceInfo_exec = require('cordova/exec');
-exports.getDeviceManufacturer = function(success, error) {
+window.getDeviceManufacturer = function(success, error) {
     deviceInfo_exec(success, error, "phonegap-plugin-deviceinfo", "getDeviceManufacturer");
 };
 
-exports.getDeviceModel = function(success, error) {
+window.getDeviceModel = function(success, error) {
     deviceInfo_exec(success, error, "phonegap-plugin-deviceinfo", "getDeviceModel");
 };
 
-exports.getDeviceId = function(success, error) {
+window.getDeviceId = function(success, error) {
     deviceInfo_exec(success, error, "phonegap-plugin-deviceinfo", "getDeviceId");
 };
 
