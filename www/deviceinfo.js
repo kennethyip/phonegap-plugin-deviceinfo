1f9724ba4bf5fab9479e4dcebbfbfb33ffda8b14
kennethyip <kenneth.yip@tungwah.org.hk>
2014-04-01 14:48:11 +0800

    Rename

--- www/deviceinfo.js
+++ www/deviceinfo.js
@@ -2,13 +2,13 @@ var exec = require('cordova/exec');
 
 var deviceInfo = {
 	getManufacturer : function(success, error) {
-		exec(success, error, "plugin-deviceinfo", "getManufacturer");
+		exec(success, error, "deviceinfo", "getManufacturer");
 	},
 	getModel : function(success, error) {
-		exec(success, error, "plugin-deviceinfo", "getModel");
+		exec(success, error, "deviceinfo", "getModel");
 	},
 	getId : function(success, error) {
-		exec(success, error, "plugin-deviceinfo", "getId");
+		exec(success, error, "deviceinfo", "getId");
 	},
 };
 
